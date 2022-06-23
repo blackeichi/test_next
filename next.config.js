a9b9d9189c62da01ef297f3bce8167de00e8a5e8
blackeichi <87626378+blackeichi@users.noreply.github.com>
2022-06-23 17:43:36 +0900

    2022.06.23

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/person/:id",
+        destination: "https://billions-api.nomadcoders.workers.dev/person/:id",
+      },
+    ];
+  },
 };
